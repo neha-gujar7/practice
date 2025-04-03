ec6953cb0d3c1148b9a8f676d04712dc5241c1dd
Neha <nehagujar79@gmail.com>
2025-04-03 23:25:40 +0530

    Create 1337A.cpp

---
+++ 1337A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+ 
+    long long t;
+    cin >> t;
+    while(t--)
+    {
+		long long a,b,c,d;
+        cin>>a>>b>>c>>d;
+ 
+        cout<<b<<" "<<c<<" "<<" "<<c<<"\n";
+ 
+ 
+    }
+    return 0;
+}
