4c49db5b14d83c8c02aa5bff9131a4e30c9de4b9
Neha <nehagujar79@gmail.com>
2025-06-02 15:35:20 +0530

    Time: 1 ms (36.28%) | Memory: 23.5 MB (17.97%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,47 +1,11 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        vector<int> a(ratings.size());
-        vector<int> b(ratings.size());
-        //for left traversal
-        a[0]=1;
-        for(int i=1;i<ratings.size();i++){  
-            if(ratings[i]>ratings[i-1]){
-                a[i]=a[i-1]+1;
-            }
-            else {
-                a[i]=1;
-            }
-            // a.push_back(a[i]);
-        }
-        
-        b[ratings.size()-1]=1;
-        for(int i=ratings.size()-2;i>=0;i--){
-            if(ratings[i]>ratings[i+1]){
-                b[i]=b[i+1]+1;            
-            }
-            else {
-                b[i]=1;
-            }
-        }
-        int sum=0;
-        for(int i=0;i<ratings.size();i++){
-            sum=sum+max(a[i],b[i]);
-        }
-       
-        return sum;
-
-
-
-
-        // from this 48/49 test cases passed
         // vector<int> a(ratings.size());
         // vector<int> b(ratings.size());
-
         // //for left traversal
         // a[0]=1;
-        // for(int i=1;i<ratings.size();i++){
-            
+        // for(int i=1;i<ratings.size();i++){  
         //     if(ratings[i]>ratings[i-1]){
         //         a[i]=a[i-1]+1;
         //     }
@@ -53,7 +17,6 @@ public:
         
         // b[ratings.size()-1]=1;
         // for(int i=ratings.size()-2;i>=0;i--){
-
         //     if(ratings[i]>ratings[i+1]){
         //         b[i]=b[i+1]+1;
                 
@@ -68,5 +31,43 @@ public:
         // }
        
         // return sum;
+
+
+
+
+        // from this 48/49 test cases passed
+        vector<int> a(ratings.size());
+        vector<int> b(ratings.size());
+
+        //for left traversal
+        a[0]=1;
+        for(int i=1;i<ratings.size();i++){
+            
+            if(ratings[i]>ratings[i-1]){
+                a[i]=a[i-1]+1;
+            }
+            else {
+                a[i]=1;
+            }
+            // a.push_back(a[i]);
+        }
+        
+        b[ratings.size()-1]=1;
+        for(int i=ratings.size()-2;i>=0;i--){
+
+            if(ratings[i]>ratings[i+1]){
+                b[i]=b[i+1]+1;
+                
+            }
+            else {
+                b[i]=1;
+            }
+        }
+        int sum=0;
+        for(int i=0;i<ratings.size();i++){
+            sum=sum+max(a[i],b[i]);
+        }
+       
+        return sum;
     }
 };
