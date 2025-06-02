5518cf287c0d7c69110a13c5b55683d4b6087cfa
Neha <nehagujar79@gmail.com>
2025-06-02 14:19:50 +0530

    Time: 0 ms (100.00%) | Memory: 23.3 MB (24.46%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,34 +1,41 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int n = ratings.size();
-        vector<int> a(n, 1);
-        vector<int> b(n, 1);
+        // from this 48/49 test cases passed
+        vector<int> a(ratings.size());
+        vector<int> b(ratings.size());
 
-        // Left to right
-        for(int i = 1; i < n; i++) {
-            if(ratings[i] > ratings[i-1]) {
-                a[i] = a[i-1] + 1;
-            } else {
-                a[i] = 1;
+        //for left traversal
+        a[0]=1;
+        for(int i=1;i<ratings.size();i++){
+            
+            if(ratings[i]>ratings[i-1]){
+                a[i]=a[i-1]+1;
             }
-        }
-
-        // Right to left
-        for(int i = n - 2; i >= 0; i--) {
-            if(ratings[i] > ratings[i+1]) {
-                b[i] = b[i+1] + 1;
-            } else {
-                b[i] = 1;
+            else {
+                a[i]=1;
             }
+            // a.push_back(a[i]);
         }
+        
+        b[ratings.size()-1]=1;
+        for(int i=ratings.size()-2;i>=0;i--){
 
-        int sum = 0;
-        for(int i = 0; i < n; i++) {
-            sum += max(a[i], b[i]);
+            if(ratings[i]>ratings[i+1]){
+                b[i]=b[i+1]+1;
+                
+            }
+            else {
+                // b.push_back(b[0]+1);
+                b[i]=1;
+            }
+            // b.push_back(1);
         }
-
+        int sum=0;
+        for(int i=0;i<ratings.size();i++){
+            sum=sum+max(a[i],b[i]);
+        }
+       
         return sum;
     }
 };
-
