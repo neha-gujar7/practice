f08fb5ead0032877ee7378c973ea3052862857cb
Neha <nehagujar79@gmail.com>
2025-06-06 21:44:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (78.95%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int sum1 = 0, sum2 = 0;
+        for (char ch : s) sum1 += ch;
+        for (char ch : t) sum2 += ch;
+        return (char)(sum2 - sum1);  // ✅ Correct fix
+    }
+};
