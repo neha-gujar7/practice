69b3485077114bae7dfb842fc8bcee4a6ec43190
Neha <nehagujar79@gmail.com>
2025-07-14 20:16:03 +0530

    Time: 2 ms (80.43%) | Memory: 45.9 MB (86.84%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -6,21 +6,39 @@ class Solution {
     }
     public void moveZeroes(int[] nums) {
         int n=nums.length;
-        int j=1;
-
-        int[] temp=new int[n];
-        int x=0;
+        
+        int j=-1;
         for(int i=0;i<n;i++){
-            if(nums[i]!=0){
-                temp[x]=nums[i];
-                x++;
+            if(nums[i]==0){
+                j=i;
+                break;
             }
         }
-        for(int i=x;i<n;i++){
-            temp[i]=0;
+ if (j == -1) return;
+        for(int i=j+1;i<n;i++){
+            if(nums[i]!=0){
+                // we cant swap , as we want to make actual value interchange , like pass-by-value, not pass-by- refrence
+                int temp=nums[j];
+                nums[j]=nums[i];
+                nums[i]=temp;
+                j++;
+            }
         }
-        for(int i=0;i<n;i++){
-            nums[i]=temp[i];
-        }
+
+//---------
+        // int[] temp=new int[n];
+        // int x=0;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]!=0){
+        //         temp[x]=nums[i];
+        //         x++;
+        //     }
+        // }
+        // for(int i=x;i<n;i++){
+        //     temp[i]=0;
+        // }
+        // for(int i=0;i<n;i++){
+        //     nums[i]=temp[i];
+        // }
     }
 }
