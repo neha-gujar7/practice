8a7565447dc0b4d594ba04386be80ae15d3bb747
Neha <nehagujar79@gmail.com>
2025-07-28 18:55:27 +0530

    Time: 2 ms (93.20%) | Memory: 55.7 MB (85.59%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five=0,ten=0;
+        int n=bills.length;
+        for(int i=0;i<n;i++){
+
+            if(bills[i]==5){
+                five++;
+            }else if(bills[i]==10){
+                if(five!=0){
+                    ten++;
+                    five--;
+                }
+                else return false;
+               
+            }
+            else{
+                if(five>=1 && ten>=1){
+                    five--;
+                    ten--;
+                }
+                else if(five>=3){
+                    five-=3;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
