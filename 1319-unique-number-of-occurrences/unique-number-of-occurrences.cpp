7a967a2c47c95af9cbdab870875cfd44b5b0b36d
Neha <nehagujar79@gmail.com>
2025-08-28 08:10:22 +0530

    Time: 3 ms (14.99%) | Memory: 11.9 MB (58.68%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int> mp;
+        for(int i:arr){
+            mp[i]++;
+        }
+        unordered_set<int> s;
+        for(auto &i:mp){
+            
+            if(s.count(i.second)) return false;
+            s.insert(i.second);
+        }
+        return true;
+    }
+};
+
+//brute -> hashmap
+//optimal-> 
