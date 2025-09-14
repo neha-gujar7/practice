60d10b673a6ade104cffb47881593888118705bc
Neha <nehagujar79@gmail.com>
2025-09-14 10:46:09 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (49.51%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int n=strs.size();
+        sort(strs.begin(),strs.end());
+
+        string s="";
+        int x=min(strs[0].size(),strs[n-1].size());
+        for(int i=0;i<x;i++){
+            if(strs[0][i]==strs[n-1][i]){
+                s+=strs[0][i];
+            }
+            else break;
+        }
+        return s;
+    }
+};
