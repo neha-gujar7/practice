bf9eb6aa495c9e35cfc9f915cd85a95285349360
Neha <nehagujar79@gmail.com>
2025-10-02 13:05:29 +0530

    Time: 2 ms (41.95%) | Memory: 8.7 MB (13.66%) - LeetSync

--- 3336-water-bottles-ii/water-bottles-ii.cpp
+++ 3336-water-bottles-ii/water-bottles-ii.cpp
@@ -3,12 +3,11 @@ public:
     int maxBottlesDrunk(int numBottles, int numExchange) {
         int c=numBottles;
         int eb=numBottles;
-        // if(numBottles<=numExchange) return 1;
-        // numBottles=0;
+      
         while(eb>=numExchange){
-            eb=eb-numExchange;
+            eb=eb-numExchange+1;
             numExchange++;
-            eb+=1;
+            // eb+=1;
             c+=1;
         }
         return c;
