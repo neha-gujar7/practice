94b57a3c27e5171e6a5347f939012c217375f3fb
Neha <nehagujar79@gmail.com>
2025-12-05 09:40:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (66.16%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char c:s){
+            if(c=='(' || c=='{' || c=='['){
+                st.push(c);   
+            }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                char x=st.top();
+                
+                if((c==']' && x!='[') || 
+                   (c==')'  && x!='(') ||
+                   (c=='}'  && x!='{')){    
+                    return false;
+
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
