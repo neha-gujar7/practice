0239d252c2a30cd96bcbab1eb67ab4d3c6f64203
Neha <nehagujar79@gmail.com>
2025-12-08 13:42:45 +0530

    Time: 2 ms (93.58%) | Memory: 7.7 MB (84.51%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.cpp
+++ 2037-count-square-sum-triples/count-square-sum-triples.cpp
@@ -4,14 +4,32 @@ public:
         int cn=0;
         // //  int c=
         // int c;
-        for(int a=1;a<=n;a++){
-            for(int b=1;b<=n;b++){
-                int c=sqrt(a*a+b*b);
-                if(c<=n  && c*c==a*a+b*b){
-                    cn++;
+        // for(int a=1;a<=n;a++){
+        //     for(int b=1;b<=n;b++){
+        //         int c=sqrt(a*a+b*b);
+        //         if(c<=n  && c*c==a*a+b*b){
+        //             cn++;
+        //         }
+        //     }
+        // }
+
+        for(int c=1;c<=n;c++){
+            int target=c*c;
+            int a=1,b=c-1;
+
+            while(a<b){
+                int sum=a*a+b*b;
+                if(sum<target) a++;
+                else if(sum>target)  b--;
+                else{
+                    a++;
+                    b--;
+                    cn+=2;
                 }
             }
+            
         }
+
         return cn;
     }
 };
