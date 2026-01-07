d654ecbcffb554419ae124d0c8845c75afa1240e
neha-gujar <nehagujar79@gmail.com>
2026-01-07 21:27:25 +0530

    Time: 6 ms (37.56%) | Memory: 78.2 MB (46.48%) - LeetSync

--- 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.cpp
+++ 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.cpp
@@ -10,37 +10,61 @@
  * right(right) {}
  * };
  */
-
 class Solution {
 public:
-    int maxProduct(TreeNode* root) {
+    // tc sc on o1
+    long long totsum = 0;
+    long long maxipd = 0;
+    static const int MOD = 1e9 + 7;
+    long long dfs(TreeNode* root) {
         if (!root)
             return 0;
-        int ts = 0;
-        // int pr = 0;
 
-        long long ans = 0;
-        static const int MOD = 1e9 + 7;
-        vector<long long> res;
-        ts = preorder(root, res, ts);
+        long long lt = dfs(root->left);
+        long long rt = dfs(root->right);
 
-        for (long  long i : res) {
-            long long cur = (ts - i) * i;
-            ans = max(ans, cur);
-        }
-        return ans % MOD;
+        long long cursum = root->val + lt + rt;
 
+        maxipd = max(maxipd, (totsum - cursum) * cursum);
+        return cursum;
     }
-
-    long long preorder(TreeNode* root, vector<long long>& res , int& ts) {
-        if (root == NULL)
-            return 0;
-
-        long long lt = preorder(root->left, res, ts); // LEFT
-        long long rt = preorder(root->right, res, ts);
-        long long cur = root->val + lt + rt;
-        res.push_back(cur);
-
-        return cur;
+    int maxProduct(TreeNode* root) {
+        totsum = dfs(root);
+        dfs(root);
+        return maxipd%MOD;
     }
 };
+
+/// having tc sc on on
+// class Solution {
+// public:
+//     int maxProduct(TreeNode* root) {
+//         if (!root)
+//             return 0;
+//         int ts = 0;
+
+//         long long ans = 0;
+//         static const int MOD = 1e9 + 7;
+//         vector<long long> res;
+//         ts = preorder(root, res, ts);
+
+//         for (long  long i : res) {
+//             long long cur = (ts - i) * i;
+//             ans = max(ans, cur);
+//         }
+//         return ans % MOD;
+
+//     }
+
+//     long long preorder(TreeNode* root, vector<long long>& res , int& ts) {
+//         if (root == NULL)
+//             return 0;
+
+//         long long lt = preorder(root->left, res, ts); // LEFT
+//         long long rt = preorder(root->right, res, ts);
+//         long long cur = root->val + lt + rt;
+//         res.push_back(cur);
+
+//         return cur;
+//     }
+// };
