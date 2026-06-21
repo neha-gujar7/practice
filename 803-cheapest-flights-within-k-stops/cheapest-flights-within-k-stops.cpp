d5164c5f9b1bb1f904b448c7df8ac7dea642de43
neha-gujar <nehagujar79@gmail.com>
2026-06-21 12:58:18 +0530

    Time: 3 ms (62.15%) | Memory: 18.1 MB (47.97%) - LeetSync

---
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst,
+                          int k) {
+
+        vector<pair<int, int>> adj[n];
+        for (auto it : flights) {
+            int u = it[0];
+            int v = it[1];
+            int cost = it[2];
+            adj[u].push_back({v, cost});
+        }
+        queue<pair<int, pair<int, int>>> q;
+
+        // start from source
+        q.push({0, {src, 0}});
+        vector<int> dist(n, INT_MAX);
+        dist[src] = 0;
+        while (!q.empty()) {
+
+            auto x = q.front();
+            q.pop();
+
+            int stops = x.first;
+            int node = x.second.first;
+            int cost = x.second.second;
+            if (stops > k)
+                continue;
+            // process neighbors of node
+            for (auto it : adj[node]) {
+                int adjNode = it.first;
+                int edgeCost = it.second;
+
+                if (cost + edgeCost < dist[adjNode]) {
+                    dist[adjNode] = cost + edgeCost;
+                    q.push({stops + 1, {adjNode, dist[adjNode]}});
+                }
+            }
+        }
+         if(dist[dst] == INT_MAX)
+            return -1;
+
+        return dist[dst];
+    }
+};
