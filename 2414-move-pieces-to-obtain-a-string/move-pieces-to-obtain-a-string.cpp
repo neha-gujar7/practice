ea3ce4f1df259595f812b95372d307120f82b31c
neha-gujar <nehagujar79@gmail.com>
2026-07-11 11:00:24 +0530

    Time: 4 ms (91.45%) | Memory: 21.8 MB (68.37%) - LeetSync

--- 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.cpp
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.cpp
@@ -3,11 +3,7 @@ public:
     bool canChange(string start, string target) {
         int n=start.size();
         int a=0,b=0;
-        // if(n==2){
-        //     if((start[0]==target[1]  && start[1]==target[0]  ) || (start[0]==target[0]  && start[1]==target[1]  )) {
-        //         return true;
-        //     }
-        // }
+       
         while(a<n || b<n){
             while(a<n && start[a]=='_') a++;
             while(b<n && target[b]=='_') b++;
